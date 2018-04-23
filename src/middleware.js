64ef0608e93a680e80486cccd24e9faffb750419
Ryan Mahoney <ryanmahoney@Ryans-MacBook.local>
2018-04-23 12:06:21 -0400

    organize validations as reduce

--- src/middleware.js
+++ src/middleware.js
@@ -1,32 +1,46 @@
 import { decryptSession, encryptSession } from "./session";
 
-const forbidden = (res, redirect, code) => {
-  res.set("x-error", code);
+const forbidden = (res, redirect) => {
   res.redirect(redirect);
   res.status(403);
 };
 
 const addSessionToRequestContext = (req, token, signingKey) => {
   req.session = decryptSession(token, signingKey);
-  return (req.session === false) ? false : true;
+  return req.session === false ? false : true;
 };
 
 const addAccessToRequestContext = (req, access) => {
   req.access = access;
-  return true;
 };
 
 export const middlewareFactory = (activityAuth, signingKey) => (
   activity,
   redirect = "/access-denied"
 ) => (req, res, next) =>
-    !req.cookies || !req.cookies.token
-      ? forbidden(res, redirect, 1)
-      : !addSessionToRequestContext(req, req.cookies.token, signingKey)
-        ? forbidden(res, redirect, 2)
-        : !req.session.roles
-          ? forbidden(res, redirect, 3)
-          : !activityAuth.check(activity, req.session.roles)
-            ? forbidden(res, redirect, 4)
-            : addAccessToRequestContext(req, activityAuth.getAccessData(req.session.roles)) && next();
+  [
+    // check for token
+    (req, res) =>
+      !req.cookies || !req.cookies.token ? res.set("x-error", 1) : true,
 
+    // check for session
+    (req, res) =>
+      !addSessionToRequestContext(req, req.cookies.token, signingKey)
+        ? res.set("x-error", 2)
+        : true,
+
+    // check for roles in session
+    (req, res) => (!req.session.roles ? res.set("x-error", 3) : true),
+
+    // check for role access to activity
+    (req, res) =>
+      !activityAuth.check(activity, req.session.roles)
+        ? res.set("x-error", 4)
+        : true
+  ].reduce((doNext, fn) => (doNext === true ? fn(req, res) : false), true) ===
+  true
+    ? addAccessToRequestContext(
+        req,
+        activityAuth.getAccessData(req.session.roles)
+      ) && next()
+    : forbidden(res, redirect);
