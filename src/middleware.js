3ba2734be5d32212148792974e24b088771e5da1
Ryan Mahoney <ryanmahoney@Ryans-MacBook.local>
2018-04-23 13:35:26 -0400

    support Authorization header request header

--- src/middleware.js
+++ src/middleware.js
@@ -10,9 +10,15 @@ const addSessionToRequestContext = (req, token, signingKey) => {
   return req.session === false ? false : true;
 };
 
-const addAccessToRequestContext = (req, access) => {
-  req.access = access;
-};
+const addAccessToRequestContext = (req, access) => (req.access = access);
+
+const getTokenFromCookieOrHeader = req =>
+  (req.token =
+    req.cookies && req.cookies.token
+      ? req.cookies.token
+      : req.header("Authorization")
+        ? req.header("Authorization").replace(/^Bearer /, "")
+        : false);
 
 export const middlewareFactory = (activityAuth, signingKey) => (
   activity,
@@ -21,11 +27,11 @@ export const middlewareFactory = (activityAuth, signingKey) => (
   [
     // check for token
     (req, res) =>
-      !req.cookies || !req.cookies.token ? res.set("x-error", 1) : true,
+      !getTokenFromCookieOrHeader(req) ? res.set("x-error", 1) : true,
 
     // check for session
     (req, res) =>
-      !addSessionToRequestContext(req, req.cookies.token, signingKey)
+      !addSessionToRequestContext(req, req.token, signingKey)
         ? res.set("x-error", 2)
         : true,
 
